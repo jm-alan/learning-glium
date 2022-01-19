445a9e937296bc214e00fb5ec83846f56f78b5f0
jm <j.maxwell.alan@gmail.com>
2022-01-19 01:16:29 -0500

    teapot go zoom

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ use glium::{
   index::PrimitiveType::TrianglesList,
   // texture,
   uniform,
+  Depth,
+  DepthTest,
   Display,
+  DrawParameters,
   IndexBuffer,
   Program,
   Surface,
@@ -44,7 +47,8 @@ fn main() {
 
   let e_loop: EventLoop<()> = EventLoop::new();
   let w_build: WindowBuilder = WindowBuilder::new();
-  let c_build: ContextBuilder<NotCurrent> = ContextBuilder::new();
+  let c_build: ContextBuilder<NotCurrent> =
+    ContextBuilder::new().with_depth_buffer(24);
 
   let display: Display = Display::new(w_build, c_build, &e_loop).unwrap();
 
@@ -57,46 +61,56 @@ fn main() {
 
   // let texture = texture::SrgbTexture2d::new(&display, composed_image).unwrap();
 
-  let shader_v = r#"
-    #version 140
+  const SHADER_V: &str = r#"
+    #version 150
 
     in vec3 position;
     in vec3 normal;
 
+    out vec3 v_normal;
+
     uniform mat4 matrix;
+    uniform mat4 perspective;
 
     void main() {
-      gl_Position = matrix * vec4(position, 1.0);
+      v_normal = transpose(inverse(mat3(matrix))) * normal;
+      gl_Position = perspective * matrix * vec4(position, 1.0);
     }
   "#;
 
-  let shader_f = r#"
+  const SHADER_F: &str = r#"
     #version 140
 
+    in vec3 v_normal;
     out vec4 color;
+    uniform vec3 u_light;
 
     void main() {
-      color = vec4(1.0, 0.0, 0.0, 1.0);
+      float brightness = dot(normalize(v_normal), normalize(u_light));
+      vec3 dark_color = vec3(0.6, 0.0, 0.0);
+      vec3 regular_color = vec3(1.0, 0.0, 0.0);
+      color = vec4(mix(dark_color, regular_color, brightness), 1.0);
     }
   "#;
 
-  let program =
-    Program::from_source(&display, shader_v, shader_f, None).unwrap();
+  let program: Program =
+    Program::from_source(&display, SHADER_V, SHADER_F, None).unwrap();
 
-  // let mut offset: f32 = 0.0;
-  // let mut op = '-';
+  let mut offset: f32 = 0.0;
+  let mut op = '-';
+  const OFFSET_BOUND: f32 = 2.0;
 
   e_loop.run(move |ev, _, control_flow| {
-    // if offset > 1.0 {
-    //   op = '-'
-    // } else if offset < -1.0 {
-    //   op = '+'
-    // };
-    // if op == '-' {
-    //   offset -= 0.002;
-    // } else if op == '+' {
-    //   offset += 0.002;
-    // }
+    if offset > OFFSET_BOUND {
+      op = '-'
+    } else if offset < -OFFSET_BOUND {
+      op = '+'
+    };
+    if op == '-' {
+      offset -= 0.02;
+    } else if op == '+' {
+      offset += 0.02;
+    }
 
     // let uniforms = uniform! {
     //   matrix: [
@@ -108,22 +122,51 @@ fn main() {
     //   tex: &texture
     // };
 
-    let matrix: [[f32; 4]; 4] = [
+    let position_matrix: [[f32; 4]; 4] = [
       [0.01, 0.0, 0.0, 0.0],
       [0.0, 0.01, 0.0, 0.0],
       [0.0, 0.0, 0.01, 0.0],
-      [0.0, 0.0, 0.0, 1.0],
+      [0.0, 0.0, offset + 3.0, 1.0],
     ];
 
+    let light: [f32; 3] = [1.0, 1.0, 0.5];
+
     let mut target = display.draw();
-    target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+    target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+
+    let perspective_matrix: [[f32; 4]; 4] = {
+      let (width, height) = target.get_dimensions();
+      let aspect_ratio = height as f32 / width as f32;
+      let fov: f32 = 3.1415926535897932384626 / 3.0;
+      let z_far = (2 ^ 10) as f32;
+      let z_near: f32 = 0.1;
+      let f: f32 = (fov / 2.0).tan();
+
+      [
+        [f * aspect_ratio, 0.0, 0.0, 0.0],
+        [0.0, f, 0.0, 0.0],
+        [0.0, 0.0, ((z_far + z_near) / (z_far - z_near)), 1.0],
+        [0.0, 0.0, (-(2.0 * z_far * z_near) / (z_far - z_near)), 0.0],
+      ]
+    };
+
+    let params: DrawParameters = DrawParameters {
+      depth: Depth {
+        test: DepthTest::IfLess,
+        write: true,
+        ..Default::default()
+      },
+      ..Default::default()
+    };
+
     target
       .draw(
         (&positions, &normals),
         &indices,
         &program,
-        &uniform! { matrix: matrix },
-        &Default::default(),
+        &uniform! { matrix: position_matrix, u_light: light, perspective: perspective_matrix },
+        &params,
       )
       .unwrap();
     target.finish().unwrap();
