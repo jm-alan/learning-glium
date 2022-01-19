307ed370c85d72dc1dfc999fc1a8f96efd098ab2
jm <j.maxwell.alan@gmail.com>
2022-01-18 23:09:48 -0500

    interim

--- src/main.rs
+++ src/main.rs
@@ -3,76 +3,92 @@ use glium::{
     event,
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
-    ContextBuilder,
+    ContextBuilder, NotCurrent,
   },
   implement_vertex,
   index::{NoIndices, PrimitiveType::TrianglesList},
-  uniform, Display, Program, Surface, VertexBuffer,
+  texture, uniform, Display, Program, Surface, VertexBuffer,
 };
-use std::time::{Duration, Instant};
+use image::{ImageBuffer, ImageFormat, Rgb};
+use std::{
+  io::Cursor,
+  time::{Duration, Instant},
+};
+
+mod teapot;
 
 fn main() {
+  let image: ImageBuffer<Rgb<u8>, Vec<u8>> = image::load(
+    Cursor::new(&include_bytes!("/Users/jm/Desktop/use-as-texture.png")),
+    ImageFormat::Png,
+  )
+  .unwrap()
+  .to_rgb8();
+  let image_dimensions = image.dimensions();
+  let composed_image: texture::RawImage2d<u8> =
+    texture::RawImage2d::from_raw_rgb_reversed(
+      &image.into_raw(),
+      image_dimensions,
+    );
+
   #[derive(Copy, Clone)]
   struct Vertex {
     position: [f32; 2],
+    tex_coords: [f32; 2],
   }
 
-  implement_vertex!(Vertex, position);
+  implement_vertex!(Vertex, position, tex_coords);
 
-  let e_loop = EventLoop::new();
-  let w_build = WindowBuilder::new();
-  let c_build = ContextBuilder::new();
+  let e_loop: EventLoop<()> = EventLoop::new();
+  let w_build: WindowBuilder = WindowBuilder::new();
+  let c_build: ContextBuilder<NotCurrent> = ContextBuilder::new();
 
-  let display = Display::new(w_build, c_build, &e_loop).unwrap();
+  let display: Display = Display::new(w_build, c_build, &e_loop).unwrap();
 
-  let indices = NoIndices(TrianglesList);
+  let indices: NoIndices = NoIndices(TrianglesList);
+
+  let positions = VertexBuffer::new(&display, &teapot::VERTICIES).unwrap();
+
+  // let texture = texture::SrgbTexture2d::new(&display, composed_image).unwrap();
 
   let shader_v = r#"
     #version 140
 
     in vec2 position;
+    in vec2 tex_coords;
+    out vec2 v_tex_coords;
 
-    uniform float t;
+    uniform mat4 matrix;
 
     void main() {
-      vec2 pos = position;
-      pos.x += t;
-      pos.y -= t;
-      gl_Position = vec4(pos, 0.0, 1.0);
+      v_tex_coords = tex_coords;
+      gl_Position = matrix * vec4(position, 0.0, 1.0);
     }
   "#;
 
   let shader_p = r#"
     #version 140
 
+    in vec2 v_tex_coords;
     out vec4 color;
 
+    uniform sampler2D tex;
+
     void main() {
-      color = vec4(0.0, 0.0, 1.0, 1.0);
+      color = texture(tex, v_tex_coords);
     }
   "#;
 
-  let vert_one = Vertex {
-    position: [0.5, 0.5],
-  };
-  let vert_two = Vertex {
-    position: [0.5, -0.5],
-  };
-  let vert_three = Vertex {
-    position: [-0.5, 0.5],
-  };
-  let triangle = vec![vert_one, vert_two, vert_three];
-  let buffer_v = VertexBuffer::new(&display, &triangle).unwrap();
-
-  let program = Program::from_source(&display, shader_v, shader_p, None).unwrap();
+  let program =
+    Program::from_source(&display, shader_v, shader_p, None).unwrap();
 
   let mut offset: f32 = 0.0;
   let mut op = '-';
 
   e_loop.run(move |ev, _, control_flow| {
-    if offset > 0.5 {
+    if offset > 1.0 {
       op = '-'
-    } else if offset < -0.5 {
+    } else if offset < -1.0 {
       op = '+'
     };
     if op == '-' {
@@ -81,6 +97,16 @@ fn main() {
       offset += 0.002;
     }
 
+    let uniforms = uniform! {
+      matrix: [
+        [offset.cos(), offset.sin(), 0.0, 0.0],
+        [-offset.sin(), offset.cos(), 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0 , 0.0, 0.0, 1.0]
+      ],
+      tex: &texture
+    };
+
     let mut target = display.draw();
     target.clear_color(1.0, 0.0, 0.0, 1.0);
     target
@@ -88,7 +114,7 @@ fn main() {
         &buffer_v,
         &indices,
         &program,
-        &uniform! {t: offset},
+        &uniforms,
         &Default::default(),
       )
       .unwrap();
